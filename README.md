6c46a86b6d6bdb3fbc0461bf42a8b339b7b4922c
Emmriz <63417146+Emmriz@users.noreply.github.com>
2022-12-05 04:12:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CALENDAR-JS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-7fmwp8)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const monthEl = document.querySelector('.date h1');
+const fullDateEl = document.querySelector('.date p');
+
+const monthInx = new Date().getMonth();
+const lastDay = new Date(new Date().getFullYear(), monthInx + 1, 0).getDate();
+console.log(lastDay);
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+monthEl.innerText = months[monthInx];
+fullDateEl.innerText = new Date().toDateString();
+
+let days = "";
+
+for (let i = 1; i <=lastDay;i++){
+    days += `<div>${i}</div>`
+}
